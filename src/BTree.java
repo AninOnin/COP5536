526edefe68fa3383a4239481b1aadfd7c9efa555
vanessa <vanessa@fics206dc1p2.cise.ufl.edu>
2017-11-13 14:08:19 -0500

    initial commit

---
+++ src/BTree.java
@@ -0,0 +1,26 @@
+
+public class BTree {
+
+	public BTree() {
+
+	}
+
+	public BTree initialize(int m) {
+		// TODO: create a new order m B+ Tree
+		return null;
+	}
+
+	public void insert() {
+		// TODO: insert(key, value)
+	}
+
+	public void search() {
+		// TODO: search(key) returns all values associated with the key
+	}
+
+	public void searchInRange() {
+		// TODO: search(key1, key2) returns (all key value pairs) such that key1 <= key
+		// <= key2
+	}
+
+}
